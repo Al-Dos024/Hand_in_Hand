16fc1c7b98456951b51afb98c1af7546fbe9a25a
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-12-22 20:10:53 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class ISEF extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: QuizScreenPhaseTwoAdult(),
+      home: OnBorading(),
     );
   }
 }
