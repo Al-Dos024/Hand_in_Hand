9f054c083abc7cd57fc310be93eb910427487870
Al-Dos024 <amjdos0amjdos0190edu@gmail.com>
2023-12-24 19:30:35 +0200

    score only increase once

--- lib/views/above 18/quiz_screen_phase_two_adult.dart
+++ lib/views/above 18/quiz_screen_phase_two_adult.dart
@@ -21,6 +21,7 @@ class _QuizScreenPhaseTwoAdultState extends State<QuizScreenPhaseTwoAdult> {
   int scoreA = 0;
   int scoreB = 0;
   bool _isPressedOn = false;
+  bool doneOnce = true;
   Answer? selectedAnswer;
 
   @override
@@ -173,59 +174,62 @@ class _QuizScreenPhaseTwoAdultState extends State<QuizScreenPhaseTwoAdult> {
     //   }
     //   i = i + 2;
     // }
-    if (rightAns[0] == 1 && rightAns[1] == 1) {
-      scoreA++;
-    }
-    if (rightAns[2] == 1 && rightAns[3] == 1) {
-      scoreA++;
-    }
-    if (rightAns[4] == 1 && rightAns[5] == 1) {
-      scoreA++;
-    }
-    if (rightAns[6] == 1 && rightAns[7] == 1) {
-      scoreA++;
-    }
-    if (rightAns[8] == 1 && rightAns[9] == 1) {
-      scoreA++;
-    }
-    if (rightAns[10] == 1 && rightAns[11] == 1) {
-      scoreA++;
-    }
-    if (rightAns[12] == 1 && rightAns[13] == 1) {
-      scoreA++;
-    }
-    if (rightAns[14] == 1 && rightAns[15] == 1) {
-      scoreA++;
-    }
-    if (rightAns[16] == 1) {
-      scoreA++;
-    }
-    if (rightAns[17] == 1 && rightAns[18] == 1) {
-      scoreB++;
-    }
-    if (rightAns[19] == 1 && rightAns[20] == 1) {
-      scoreB++;
-    }
-    if (rightAns[21] == 1 && rightAns[22] == 1) {
-      scoreB++;
-    }
-    if (rightAns[23] == 1 && rightAns[24] == 1) {
-      scoreB++;
-    }
-    if (rightAns[25] == 1 && rightAns[26] == 1) {
-      scoreB++;
-    }
-    if (rightAns[27] == 1 && rightAns[28] == 1) {
-      scoreB++;
-    }
-    if (rightAns[29] == 1 && rightAns[30] == 1) {
-      scoreB++;
-    }
-    if (rightAns[31] == 1 && rightAns[32] == 1) {
-      scoreB++;
-    }
-    if (rightAns[33] == 1 && rightAns[34] == 1) {
-      scoreB++;
+    if (doneOnce == true) {
+      if (rightAns[0] == 1 && rightAns[1] == 1) {
+        scoreA++;
+      }
+      if (rightAns[2] == 1 && rightAns[3] == 1) {
+        scoreA++;
+      }
+      if (rightAns[4] == 1 && rightAns[5] == 1) {
+        scoreA++;
+      }
+      if (rightAns[6] == 1 && rightAns[7] == 1) {
+        scoreA++;
+      }
+      if (rightAns[8] == 1 && rightAns[9] == 1) {
+        scoreA++;
+      }
+      if (rightAns[10] == 1 && rightAns[11] == 1) {
+        scoreA++;
+      }
+      if (rightAns[12] == 1 && rightAns[13] == 1) {
+        scoreA++;
+      }
+      if (rightAns[14] == 1 && rightAns[15] == 1) {
+        scoreA++;
+      }
+      if (rightAns[16] == 1) {
+        scoreA++;
+      }
+      if (rightAns[17] == 1 && rightAns[18] == 1) {
+        scoreB++;
+      }
+      if (rightAns[19] == 1 && rightAns[20] == 1) {
+        scoreB++;
+      }
+      if (rightAns[21] == 1 && rightAns[22] == 1) {
+        scoreB++;
+      }
+      if (rightAns[23] == 1 && rightAns[24] == 1) {
+        scoreB++;
+      }
+      if (rightAns[25] == 1 && rightAns[26] == 1) {
+        scoreB++;
+      }
+      if (rightAns[27] == 1 && rightAns[28] == 1) {
+        scoreB++;
+      }
+      if (rightAns[29] == 1 && rightAns[30] == 1) {
+        scoreB++;
+      }
+      if (rightAns[31] == 1 && rightAns[32] == 1) {
+        scoreB++;
+      }
+      if (rightAns[33] == 1 && rightAns[34] == 1) {
+        scoreB++;
+      }
+      doneOnce = false;
     }
     return AlertDialog(
       title: Text('scoreA is $scoreA , scoreB is $scoreB '),
@@ -246,6 +250,7 @@ class _QuizScreenPhaseTwoAdultState extends State<QuizScreenPhaseTwoAdult> {
                       scoreA = 0;
                       scoreB = 0;
                       selectedAnswer = null;
+                      doneOnce = true;
                       // rightAns = List.filled(35, 0);
                     });
                   },
@@ -262,6 +267,7 @@ class _QuizScreenPhaseTwoAdultState extends State<QuizScreenPhaseTwoAdult> {
                       scoreA = 0;
                       scoreB = 0;
                       selectedAnswer = null;
+                      doneOnce = true;
                       // rightAns = List.filled(35, 0);
                     });
                   },
